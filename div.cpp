7f4299c19b02e7187c89b37412126a642cfd61f4
2016IPG-032 <roysugata8@gmail.com>
2018-04-21 15:54:38 +0530

    first.cal

---
+++ div.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    float a,b;
+    float c;
+    if(b==0)
+    {
+        cout<<"not possible";
+    }
+    cin>>a>>b;
+    c=a/b;
+    cout<<c;
+
+    return 0;
+}
+
+
+
